5b720fa525ef2ce0049aad2ab91c321170e7da78
Josezin0910 <gomes.silva.jose0910@escola.pr.gov.br>
2026-08-18 08:40:35 -0300

    aula 03

--- main.js
+++ main.js
@@ -23,3 +23,14 @@ function aumentaTamanho() {
 }
 
 const campoSenha = document.querySelector('#campo-senha')
+
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+
+function geraSenha(){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    console.log(numeroAleatorio);
+}
+
+campoSenha.value = letrasMaiusculas;
+
+
